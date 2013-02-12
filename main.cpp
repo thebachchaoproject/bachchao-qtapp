ba8896ba818d2c63a2d27cc32fd61d102588c53c
smita <smita@ubuntu.ubuntu-domain>
2013-02-12 10:37:52 +0100

    Workaround to fix camera crashing: bug in QtMobility

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <QtGui/QApplication>
+#include <QtOpenGL/QGLWidget>
+
 #include "qmlapplicationviewer.h"
 #include "location.h"
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
-    QScopedPointer<QApplication> app(createApplication(argc, argv));
+    QApplication *app(createApplication(argc, argv));
 
     QmlApplicationViewer viewer;
+    viewer.setViewport(new QGLWidget());
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
     viewer.setMainQmlFile(QLatin1String("qml/bachao/main.qml"));
     viewer.showExpanded();
